522a0a71d7ffcce3ba8ad5e5d7633467940fdb5d
keel-210 <crafters_touhou@yahoo.co.jp>
2018-04-06 01:05:21 +0900

    SteamVR_Plugin
    VR用アセットと仮置きクラス

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+	public bool IsHandGripping, IsFootGripping;
+	public Vector3 GripPosition,FootGripPosition;
+	bool IsTriggered, IsPadTouched;
+	[SerializeField] SteamVR_TrackedObject HandDevice,FootDevice;
+	void Start ()
+	{
+		HandDevice = GetComponent<SteamVR_TrackedObject> ();
+	}
+	void Update ()
+	{
+		var device = SteamVR_Controller.Input ((int) HandDevice.index);
+		IsTriggered = device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger) || device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger);
+		IsPadTouched = device.GetPressDown (SteamVR_Controller.ButtonMask.Touchpad) || device.GetPress (SteamVR_Controller.ButtonMask.Touchpad);
+		if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger))
+		{
+			IsTriggered = false;
+			IsHandGripping = false;
+		}
+		if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Touchpad))
+		{
+			IsPadTouched = false;
+			IsFootGripping = false;
+		}
+	}
+	void OnTriggerStay ()
+	{
+		if (!IsHandGripping)
+		{
+			if (IsTriggered)
+			{
+				IsHandGripping = true;
+				GripPosition = transform.position;
+			}
+		}
+		if (!IsFootGripping)
+		{
+			if (IsPadTouched)
+			{
+				IsFootGripping = true;
+				FootGripPosition = FootDevice.transform.position;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] HandController RightDevice,LeftDevice;
+	void Start ()
+	{
+
+	}
+	void Update ()
+	{
+		if(RightDevice.IsHandGripping && LeftDevice.IsHandGripping)
+		{
+			DowbleGrip();
+		}
+		else if(RightDevice.IsHandGripping || LeftDevice.IsHandGripping)
+		{
+			SingleGrip();
+		}
+		else
+		{
+			Fall();
+		}
+	}
+	void DowbleGrip()
+	{
+
+	}
+	void SingleGrip()
+	{
+
+	}
+	void Fall()
+	{
+
+	}
+}

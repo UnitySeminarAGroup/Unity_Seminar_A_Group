dbf1a5d00d412821a63710c45c3796dd1d31cfa6
keel-210 <crafters_touhou@yahoo.co.jp>
2018-04-06 17:06:15 +0900

    Base Grip System_Ver1.0
    たぶんこれでひとまず動けるはず実機確認はしてない

--- Assets/Scripts/FootController.cs
+++ Assets/Scripts/FootController.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FootController : MonoBehaviour {
+public class FootController : MonoBehaviour
+{
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    void Update()
+    {
+
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,36 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] HandController RightDevice,LeftDevice;
-	void Start ()
-	{
-
-	}
-	void Update ()
-	{
-		if(RightDevice.IsHandGripping && LeftDevice.IsHandGripping)
-		{
-			DowbleGrip();
-		}
-		else if(RightDevice.IsHandGripping || LeftDevice.IsHandGripping)
-		{
-			SingleGrip();
-		}
-		else
-		{
-			Fall();
-		}
-	}
-	void DowbleGrip()
-	{
-
-	}
-	void SingleGrip()
-	{
-
-	}
-	void Fall()
-	{
-
-	}
+    Rigidbody rigidbody;
+    [SerializeField] HandController RightDevice, LeftDevice;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (RightDevice.IsHandGripping && LeftDevice.IsHandGripping)
+        {
+            DowbleGrip();
+        }
+        else if (RightDevice.IsHandGripping || LeftDevice.IsHandGripping)
+        {
+            if (RightDevice.IsHandGripping)
+            {
+                SingleGrip(RightDevice);
+            }
+            else
+            {
+                SingleGrip(LeftDevice);
+            }
+        }
+        else
+        {
+            Fall();
+        }
+    }
+    void DowbleGrip()
+    {
+        rigidbody.useGravity = false;
+        Vector3 RightOffset = RightDevice.transform.position - RightDevice.GripPosition;
+        Vector3 LeftOffset = RightDevice.transform.position - LeftDevice.GripPosition;
+        Vector3 AveOffset = (RightOffset + LeftOffset) / 2;
+        rigidbody.position = rigidbody.position + AveOffset;
+    }
+    void SingleGrip(HandController device)
+    {
+        rigidbody.useGravity = false;
+        Vector3 offset = device.transform.position - device.GripPosition;
+        rigidbody.position = rigidbody.position + offset;
+    }
+    void Fall()
+    {
+        rigidbody.useGravity = true;
+    }
 }

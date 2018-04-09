ea93d69be9edc4691f5f96acc77b787c57dcc765
Ryosuke-Futatsumori <38063794+Ryosuke-Futatsumori@users.noreply.github.com>
2018-04-09 10:53:31 +0900

    UIver1.03
    修正

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
-
+    public Text ScoreLabel;
+    
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,7 @@ public class UIController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // スコアラベルを更新
+        int score = CalcScore();
 	}
 }

9eab44ed1103f7767e26f57e63b9ef29f0d6cf22
Ryosuke-Futatsumori <38063794+Ryosuke-Futatsumori@users.noreply.github.com>
2018-04-09 14:41:47 +0900

    UIver1.1
    ラベル仮置き
    時間のみ変化する状態

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -20,16 +20,26 @@ public class UIController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        // スコアを更新
+        // int score =;
+        // ScoreLabel.txt = "Score;" + score;
+
+
         // 残り時間を更新
         if (seconds >= 0f)
         {
             seconds -= Time.deltaTime;
             if (seconds != oldseconds)
             {
-                LimitTimeLabel.text = "残り時間:" + seconds + "秒";
-
+                LimitTimeLabel.text = "残り時間:" + Mathf.Ceil(seconds) + "秒";
             }
+            oldseconds = seconds;
+        }else{
+            seconds = 0f;
         }
         
+        // 頭と両足の距離を更新
+        int 
+
 	}
 }

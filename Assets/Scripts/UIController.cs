1fc737834d1b994cbaaed83a04cb5e45a156ded1
Ryosuke-Futatsumori <38063794+Ryosuke-Futatsumori@users.noreply.github.com>
2018-04-09 13:33:05 +0900

    UIver1.04
    修正

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,15 +5,31 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
     public Text ScoreLabel;
-    
+    public Text LimitTimeLabel;
+    public Text DistanceLabel;
+
+    private float seconds;
+    private float oldseconds;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        seconds = 120f;
+        oldseconds = 120f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        // スコアラベルを更新
-        int score = CalcScore();
+
+        // 残り時間を更新
+        if (seconds >= 0f)
+        {
+            seconds -= Time.deltaTime;
+            if (seconds != oldseconds)
+            {
+                LimitTimeLabel.text = "残り時間:" + seconds + "秒";
+
+            }
+        }
+        
 	}
 }

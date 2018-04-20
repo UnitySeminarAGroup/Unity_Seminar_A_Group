483a65f9eb725d7b6d600babae9af2390e002eb4
keel-210 <crafters_touhou@yahoo.co.jp>
2018-04-20 17:05:38 +0900

    UIver2.7
    時間切れ時シーン遷移の遅延を実装

--- Assets/Scripts/LimitTimeManager.cs
+++ Assets/Scripts/LimitTimeManager.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LimitTimeManager : MonoBehaviour {
+public class LimitTimeManager : MonoBehaviour
+{
+    [SerializeField]
+    float DelayTime;
     float LimitTime;
     UIController ui;
-	// Use this for initialization
-	void Start () {
+    void Start()
+    {
         ui = FindObjectOfType<UIController>();
         LimitTime = ui.seconds;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Update()
+    {
         if (LimitTime < 0.01f)
         {
-            SceneManager.LoadScene("Title");
+            StartCoroutine(this.DelayMethod(DelayTime, () =>
+            {
+                SceneManager.LoadScene("Title");
+            }));
         }
-	}
+    }
 }

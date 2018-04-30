bce97e4dacc789abb29375f06a0a9dba531b3f53
Ryosuke-Futatsumori <38063794+Ryosuke-Futatsumori@users.noreply.github.com>
2018-04-30 16:52:28 +0900

    UI Ver3.4
    タイトルロゴを仮設置
    ゴール時のSEと宝箱のアニメーションを仮実装

---
+++ Assets/Scripts/GoalActionController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalActionController : MonoBehaviour {
+
+	[SerializeField] Animator goalAnimator;
+	[SerializeField] AudioSource goalSE;
+	void OntriggerEnter(Collider other){
+		if(!goalSE.isPlaying){
+			goalSE.Play();
+			goalAnimator.SetBool("Trigger",true);
+		}
+
+	}
+}

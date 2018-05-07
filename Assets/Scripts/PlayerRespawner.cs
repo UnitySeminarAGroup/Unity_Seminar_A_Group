fb919ecd6d951ea6713d2d25d45c68b1b4bfdf03
keel-210 <crafters_touhou@yahoo.co.jp>
2018-05-07 18:16:29 +0900

    Stage_KEEL Ver1.0

---
+++ Assets/Scripts/PlayerRespawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawner : MonoBehaviour
+{
+	void Update ()
+	{
+		if (transform.position.y < -10)
+		{
+			transform.position = Vector3.zero;
+		}
+	}
+}

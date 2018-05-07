9a146c584fbb1ceaad75bca1e3b2d7a4ae9fc55d
Harris-1227 <38063794+Harris-1227@users.noreply.github.com>
2018-05-07 15:09:13 +0900

    UI Ver3.5
    パーティクルのインスタンス化

---
+++ Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour {
+	public GameObject effectPrefab;
+	public Vector3 effectRotation;
+
+	void OnDisable()
+	{
+		if(!effectPrefab)
+		{
+			Instantiate(
+				effectPrefab,
+				this.transform.position,
+				Quaternion.Euler(effectRotation)
+			);
+		}
+	}
+}

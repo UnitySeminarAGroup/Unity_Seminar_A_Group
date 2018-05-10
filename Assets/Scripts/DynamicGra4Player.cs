ad05739b0de38320757e469d58d1f27c2ac186fe
keel-210 <crafters_touhou@yahoo.co.jp>
2018-05-10 15:31:07 +0900

    Stage_KEEL Ver1.3

---
+++ Assets/Scripts/DynamicGra4Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicGra4Player : MonoBehaviour
+{
+	[SerializeField] DynamicGravity dynamicGravity;
+	[SerializeField] HandController rightDevice, leftDevice;
+	void Update ()
+	{
+		if (!rightDevice.IsHandGripping && !leftDevice.IsHandGripping)
+		{
+			dynamicGravity.enabled = true;
+		}
+		else
+		{
+			dynamicGravity.enabled = false;
+		}
+	}
+}

1aa12df0226e1f216445de68d3df5af6e81feeb6
keel-210 <crafters_touhou@yahoo.co.jp>
2018-05-11 17:25:18 +0900

    Stage_KEEL Ver1.7

--- Assets/Scripts/GoalActionController.cs
+++ Assets/Scripts/GoalActionController.cs
@@ -7,12 +7,13 @@ public class GoalActionController : MonoBehaviour
 
     [SerializeField] Animator goalAnimator;
     [SerializeField] AudioSource goalSE;
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter (Collider other)
     {
         if (!goalSE.isPlaying)
         {
-            goalSE.Play();
-            goalAnimator.SetBool("Trigger", true);
+            goalSE.Play ();
+            goalAnimator.SetBool ("Trigger", true);
+            GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody> ().isKinematic = true;
         }
 
     }
